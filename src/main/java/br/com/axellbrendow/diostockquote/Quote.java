ebb40082cc10badcedb2c2641fad1642cbff775e
axell-brendow <breno.axel@gmail.com>
2021-09-21 10:53:41 -0300

    Create Quote entity

---
+++ src/main/java/br/com/axellbrendow/diostockquote/Quote.java
@@ -0,0 +1,30 @@
+package br.com.axellbrendow.diostockquote;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "QUOTE")
+public class Quote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String symbol;
+    private Double openValue;
+    private Double closeValue;
+    private Date timestamp;
+}
