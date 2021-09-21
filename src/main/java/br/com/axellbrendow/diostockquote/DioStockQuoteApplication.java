d4868703fd1370539b0de186103dd7d8bb0b904c
axell-brendow <breno.axel@gmail.com>
2021-09-21 10:14:02 -0300

    Initial upload

---
+++ src/main/java/br/com/axellbrendow/diostockquote/DioStockQuoteApplication.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diostockquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioStockQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioStockQuoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/axellbrendow/diostockquote/DioStockQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diostockquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioStockQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
