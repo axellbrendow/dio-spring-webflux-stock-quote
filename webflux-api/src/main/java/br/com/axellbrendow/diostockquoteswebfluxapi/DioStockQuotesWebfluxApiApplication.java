007fd0fd08e5cb938e4f07703d0076976653f82f
axell-brendow <breno.axel@gmail.com>
2021-09-22 11:33:06 -0300

    Starts a new backend using Spring WebFlux

---
+++ webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/DioStockQuotesWebfluxApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diostockquoteswebfluxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioStockQuotesWebfluxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioStockQuotesWebfluxApiApplication.class, args);
+	}
+
+}
---
+++ webflux-api/src/test/java/br/com/axellbrendow/diostockquoteswebfluxapi/DioStockQuotesWebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diostockquoteswebfluxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioStockQuotesWebfluxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
