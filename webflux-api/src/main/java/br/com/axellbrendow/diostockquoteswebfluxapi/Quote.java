d328badd894e726c3f5249136da9b3d58123096f
axell-brendow <breno.axel@gmail.com>
2021-09-23 09:35:38 -0300

    Create Quote entity

---
+++ webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/Quote.java
@@ -0,0 +1,24 @@
+package br.com.axellbrendow.diostockquoteswebfluxapi;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Quote {
+    @Id
+    private Long id;
+    private String symbol;
+    private Double openValue;
+    private Double closeValue;
+    private LocalDateTime timestamp;
+}
