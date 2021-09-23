ee808a4e53a43951ee9813c4e57dbac13c49c77c
axell-brendow <breno.axel@gmail.com>
2021-09-23 10:04:22 -0300

    Change QuoteGenerator to generate quotations infinitely

--- webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/QuoteGenerator.java
+++ webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/QuoteGenerator.java
@@ -1,39 +1,59 @@
 package br.com.axellbrendow.diostockquoteswebfluxapi;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
 
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import reactor.core.publisher.Flux;
 
 @Log4j2
-@Component
-public class QuoteGenerator implements ApplicationRunner {
-    @Autowired
-    private QuoteRepository repository;
+@Service
+@Transactional
+@AllArgsConstructor
+public class QuoteGenerator {
+    private final QuoteRepository repository;
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        log.info("Starting data insertion");
+    @PostConstruct
+    public void init() {
+        Flux.generate(() -> {
+            log.info("Starting data insertion");
+            return initialQuote();
+        }, (state, sink) -> {
+            sink.next(state);
+            return createNewQuote(state);
+        })
+        .delaySubscription(Duration.ofMillis(3000))
+        .subscribe();
+    }
 
+    private Quote createNewQuote(Quote previousQuote) {
+		var newQuote = Quote.builder()
+			.symbol(previousQuote.getSymbol())
+			.openValue(previousQuote.getOpenValue() + new RandomDataGenerator().nextUniform(-0.1, 0.1))
+			.closeValue(previousQuote.getCloseValue() + new RandomDataGenerator().nextUniform(-0.1, 0.1))
+			.timestamp(LocalDateTime.now())
+			.build();
+		repository.save(newQuote).subscribe();
+        log.info("Inserting {}", newQuote);
+        return newQuote;
+    }
+
+    private Quote initialQuote() {
         var quote = Quote.builder()
             .openValue(0.2)
             .closeValue(0.2)
             .symbol("TESTE")
             .timestamp(LocalDateTime.now())
             .build();
-
-        repository.save(quote)
-            .subscribe(
-                f -> repository.findAll()
-                    .subscribe(
-                        q -> log.info("Quote {}", q),
-                        e -> log.error("findAll Error: {}", e.getMessage())
-                    ),
-                e -> log.error("save Error: {}", e.getMessage())
-            );
+		repository.save(quote).subscribe();
+        log.info("Inserting {}", quote);
+        return quote;
     }
 }
