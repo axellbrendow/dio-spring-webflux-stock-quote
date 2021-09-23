b2a8d4174cb69601e5966abee2934839f98809d3
axell-brendow <breno.axel@gmail.com>
2021-09-23 09:36:07 -0300

    Change table name to quotes

--- webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/Quote.java
+++ webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/Quote.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table("quotes")
 public class Quote {
     @Id
     private Long id;
---
+++ webflux-api/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+DELETE FROM quotes;
+
+insert into quotes (id, close_value, open_value, symbol, timestamp)
+values (null, 0.2222222, 0.2222222, 'TESTE', '2021-09-22T14:01:46.665Z');
