1534dca838045eb97e7a9e06942141dac4453f22
axell-brendow <breno.axel@gmail.com>
2021-09-24 00:42:57 -0300

    Add CORS configuration

--- webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/WebConfig.java
+++ webflux-api/src/main/java/br/com/axellbrendow/diostockquoteswebfluxapi/WebConfig.java
@@ -5,13 +5,23 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 @EnableWebFlux
-public class WebConfig {
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .maxAge(3600);
+    }
+
     @Bean
     public RouterFunction<ServerResponse> routeQuotes(QuoteHandler quoteHandler) {
         return route(GET("/quotes"), quoteHandler::getAll)
